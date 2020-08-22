01c7489c495fc0025fa8e5cc8f52b50c086e5085
Nguyen Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2020-08-22 17:42:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js")
 
 const client = new Discord.Client();
 
+const prefix = "beepboop ";
+
 client.once('ready', () => {
 	console.log("BeepBoop is online");
 })
@@ -9,18 +11,21 @@ client.once('ready', () => {
 
 client.on('message', message =>{
 
-	const args = message.content;
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+
+	const args = message.content.slice(prefix.length);
 	const command = args.toLowerCase();
 
-	if (command === 'beepboop lovetest'){
-		var a = Math.floor(Math.random() * 101) + 0;
-		var b = a.toString();
-		message.channel.send(b + "% of love");
-	} else if (command == "beepboop showdate"){
+	console.log(command);
+
+
+	if (command == "showdate"){
 		var date = new Date();
-		var result = date.getDate().toString() + "-" + date.getMonth().toString() + "-" + date.getFullYear().toString();
+		var special = date.getMonth() + 1;
+		var result = date.getDate().toString() + "-" + special.toString() + "-" + date.getFullYear().toString();
 		message.channel.send(result);
-	} else if (command == "beepboop sendsomegif"){
+	} else if (command == "sendsomegif"){
 		var a = Math.floor(Math.random() * 3) + 0;
 		if (a == 0){
 			message.channel.send("https://media.giphy.com/media/yFQ0ywscgobJK/giphy.gif");
@@ -29,14 +34,14 @@ client.on('message', message =>{
 		} else if (a == 2){
 			message.channel.send("https://media.giphy.com/media/emWySpOLFLUAM/giphy.gif");
 		}
-	} else if (command == "beepboop tellastory"){
+	} else if (command == "tellastory"){
 		var a = Math.floor(Math.random() * 2) + 0;
 		if (a == 0){
 			message.channel.send('Once upon a time, there was a Beepboop-20 talking to a Beepboop-26. BeepBoop-20 said "beep beep boop beep". And the BeepBoop-26 said "boop beep beep boop beep".');
 		} else {
 			message.channel.send('"Why did you betray me Sabrina ?" - a man said. Then, the woman just reply with a bark, and let another person pet her.');
 		}
-	} else if (command == "beepboop saysomething"){
+	} else if (command == "saysomething"){
 		var a = Math.floor(Math.random() * 5) + 0;
 		if (a == 0){
 			message.channel.send('Beep boop boop beep');
@@ -49,24 +54,40 @@ client.on('message', message =>{
 		} else if (a == 4){
 			message.channel.send("Hello there, human thingy kinda stuffs...");
 		}
-	} else if (command == "beepboop help"){
-		message.channel.send("Beep boop beep boop, hello fellow human. \nTo use me, type: beepboop [command] \n\nCommands: \nsaysomething    tellastory    sendsomegif    lovetest    showdate    help \n\nSpecial commands: cf [side] \nhead    tail");
-	} else if (command == "beepboop cf head"){
+	} else if (command == "help"){
+		message.channel.send("Beep boop beep boop, hello fellow human. \nTo use me, type: beepboop [command] \n\nCommands: \nsaysomething    laugh    tellastory    sendsomegif    goodbye    hello    showdate    help \n\nSpecial commands: cf [side] \nhead    tail");
+	} else if (command == "cf head"){
 		var a = Math.floor(Math.random() * 2) + 0;
 		if (a==0) {
 			message.channel.send("The coin goes for the head \nYou win!");
 		} else {
 			message.channel.send("The coin goes for the tail \nYou lose!");
 		}
-	} else if (command == "beepboop cf tail"){
+	} else if (command == "cf tail"){
 		var a = Math.floor(Math.random() * 2) + 0;
 		if (a==0) {
 			message.channel.send("The coin goes for the tail \nYou win!");
 		} else {
 			message.channel.send("The coin goes for the head \nYou lose!");
 		}
+	} else if (command == "goodbye") {
+		message.channel.send("Goodbye fellow human!");
+	} else if (command == "hello") {
+		message.channel.send("Hello fellow human!");
+	} else if (command == "iluvu") {
+		message.channel.send("*Hug, hold hand, kiss");
+	} else if (command == "laugh"){
+		var a = Math.floor(Math.random() * 2) + 0;
+		if (a == 0){
+			message.channel.send('Lmao');
+		} else {
+			message.channel.send('LOL');
+		}
+	} else if (command.substr(0,8) == 'lovetest') {
+		var a = Math.floor(Math.random() * 101) + 0;
+		var b = a.toString();
+		message.channel.send(b + "% of love");
 	}
 });
-
 
 client.login(/*Enter token here*/);
